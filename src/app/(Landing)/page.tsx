f51f60dc01a7f53d29a2231d8a074c005c16ed05
Aayush Pagare <aayushpagare21@gmail.com>
2024-09-28 07:57:29 +0530

    feat: create landing page without image.

--- src/app/(Landing)/page.tsx
+++ src/app/(Landing)/page.tsx
@@ -1,81 +1,56 @@
 import { Button } from "@/app/components/ui/button";
 import React from "react";
-import Image from "next/image";
-
-const Page = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <div className="flex flex-col h-screen bg-[#F7F4ED] text-black">
-      {children}
-    </div>
-  );
-};
-
-const navitems = [
-  { content: "Our Story" },
-  { content: "Membership" },
-  { content: "Write" },
-  { content: "Sign In" },
-];
-const Navbar = () => {
-  return (
-    <div className="flex justify-center border border-b-black">
-      <nav className="flex justify-between w-[90%] xl:w-[80%] 2xl:w-[60%]">
-        <div className="font-bold text-[2rem] font-[family-name:var(--font-gtsuper-medium)] my-3 tracking-normal">
-          Aspirant
-        </div>
-        <div>
-          <ul className="flex gap-6 text-[13px] font-[family-name:var(--font-sohneone)]">
-            {navitems.map((item, index) => {
-              return (
-                <li key={index} className="my-7 hidden sm:block">
-                  <a href=""> {item.content} </a>
-                </li>
-              );
-            })}
-            <li className="my-5">
-              <Button className="px-4 py-2"> Get Started </Button>
-            </li>
-          </ul>
-        </div>
-      </nav>
-    </div>
-  );
-};
-
-const HeroSection = () => {
-  return (
-    <div>
-      <div className="flex justify-center border border-b-black items-center">
-        <div className="flex flex-col gap-12 2xl:w-[60%] xl:w-[80%] w-[90%] my-[5rem]">
-          <div className="text-[4rem] leading-[4rem] max-w-[200px] sm:leading-[6rem] sm:text-[7rem] sm:max-w-[700px] font-[family-name:var(--font-gtsuper-medium)] m-0 p-0">
-            {`Aspirant stories & ideas`}
-          </div>
-          <div className="text-[22px]">
-            {" "}
-            A place to read, write and deepen your understanding{" "}
-          </div>
-          <Button className="w-[196px] h-[46px] text-[1.2rem] bg-[#1A8917] lg:bg-black">
-            {" "}
-            Start reading{" "}
-          </Button>
-        </div>
-      </div>
-      <Image
-        src="/images/lbsnaa.jpeg"
-        alt="image goes here"
-        height="300"
-        width="300"
-        className="hidden lg:block absolute top-[76px] rounded-full right-[-50px] h-[500px] w-[300px] xl:right-[130px] xl:rounded-none xl:w-[450px] xl:h-[530px]"
-      />
-    </div>
-  );
-};
-
 export default function LandingPage() {
   return (
-    <Page>
-      <Navbar />
-      <HeroSection />
-    </Page>
+    <div className="bg-[#F7F4ED] h-[100vh] flex flex-col items-center">
+      <header>
+        <nav className="flex justify-between w-[90vw] 2xl:w-[50vw] p-4 items-center">
+          <div className="font-[family-name:var(--font-gtsuper-medium)] text-[2rem] font-bold">
+            {" "}
+            Aspirant
+          </div>
+          <div className="flex justify-around gap-8">
+            <ul className="font-[family-name:var(--font-sohneone)] text-[14px] items-center md:flex gap-6 hidden">
+              <li> Our Story</li>
+              <li> Membership</li>
+              <li> Write</li>
+              <li> Sign in</li>
+            </ul>
+            <div className="flex items-center">
+              <Button className="py-2 px-4"> Get started </Button>
+            </div>
+          </div>
+        </nav>
+      </header>
+      <div className="border border-b-black w-full"></div>
+      <section className="w-[90vw] 2xl:w-[50vw] h-[80vh] flex justify-between">
+        <div className="flex flex-col justify-center my-[5rem] gap-12">
+          <div className="font-[family-name:var(--font-gtsuper-medium)] tracking-normal text-[5rem] leading-[4rem] lg:text-[7rem] lg:leading-[6rem]">
+            Aspirant <br /> {"stories & ideas"}
+          </div>
+          <div className="font-[family-name:var(--font-sohneone)] text-[1.5rem] leading-none">
+            A place to read, write and deepen your understanding
+          </div>
+          <Button className="py-2 px-8 text-[1.2rem] w-[13rem] bg-[#1A8917] lg:bg-black">
+            {" "}
+            Get started{" "}
+          </Button>
+        </div>
+      </section>
+      <div className="border border-b-black w-full"></div>
+      <footer className="w-full lg:w-[90vw] p-4 bg-black text-secondary lg:text-primary lg:bg-inherit">
+        <ul className="font-[family-name:var(--font-sohneone)] text-[12px] lg:text-[#6B6B6B] flex items-center justify-start lg:justify-center gap-6">
+          <li> Help</li>
+          <li className="hidden lg:block"> Status</li>
+          <li> About</li>
+          <li className="hidden lg:block"> Careers</li>
+          <li className="hidden lg:block"> Press</li>
+          <li className="hidden lg:block"> Blog</li>
+          <li> Privacy</li>
+          <li className="hidden lg:block"> Text to speech</li>
+          <li> Terms</li>
+        </ul>
+      </footer>
+    </div>
   );
 }
