6ce876292960278b6eb4ba4f2d365d79ed2e7e7a
Aayush Pagare <aayushpagare21@gmail.com>
2024-09-28 22:04:31 +0530

    fix: prod font issue

--- src/app/(Landing)/layout.tsx
+++ src/app/(Landing)/layout.tsx
@@ -6,15 +6,16 @@ const gtSuper = localFont({
   src: "../../../public/fonts/GT-Super/GT-Super-Display-Regular-Trial.woff2",
   variable: "--font-gtsuper-medium",
   weight: "100 900",
+  display: "swap",
 });
 
 const sohne = localFont({
   src: "../../../public/fonts/sohne/sohneone.woff2",
   variable: "--font-sohneone",
   weight: "100 900",
+  display: "swap",
 });
 
-// Add the viewport meta tag to the metadata object
 export const metadata: Metadata = {
   title: "Be an aspirant",
   description: "Created with ❤️ by Aayush Pagare.",
@@ -29,6 +30,20 @@ export default function RootLayout({
       <html lang="en">
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link
+            rel="preload"
+            href="../../../public/fonts/GT-Super/GT-Super-Display-Regular-Trial.woff2"
+            as="font"
+            type="font/woff2"
+            crossOrigin="anonymous"
+        />
+        <link
+            rel="preload"
+            href="../../../public/fonts/sohne/sohneone.woff2"
+            as="font"
+            type="font/woff2"
+            crossOrigin="anonymous"
+        />
       </head>
       <body className={`${gtSuper.variable} ${sohne.variable}`}>
       {children}
