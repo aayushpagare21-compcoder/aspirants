ecf6fdfaa9fe14bf5ba1ad86f2b0452b4b4cd6b8
Aayush Pagare <aayushpagare21@gmail.com>
2024-10-10 00:17:38 +0530

    fix close icon

--- src/app/(home)/@modal/(.)login/page.tsx
+++ src/app/(home)/@modal/(.)login/page.tsx
@@ -5,6 +5,8 @@ import { Button } from "@/app/components/ui/button";
 import { MailIcon } from "lucide-react";
 import { GoogleIcon } from "@/app/components/shared/Logo/GoogleIcon";
 import { useState } from "react";
+import { DialogClose, DialogContent } from "@radix-ui/react-dialog";
+import { CloseIcon } from "next/dist/client/components/react-dev-overlay/internal/icons/CloseIcon";
 
 enum ModalState {
   LOGIN = "LOGIN",
@@ -97,23 +99,31 @@ export default function InterceptedLogin() {
   const onRegisterPage = modalState == ModalState.REGISTER;
   return (
     <>
-      <Dialog>
-        <div className="fixed inset-0 z-40 bg-[#ffffff] bg-opacity-50 backdrop-blur-sm" />
-        <div className="fixed inset-0 z-50 flex items-center justify-center">
-          <div className="flex h-full w-full flex-col justify-center gap-10 bg-white md:h-[42rem] md:w-[38rem] md:rounded-lg md:px-14 md:py-16 md:shadow-lg">
-            <Header onRegisterPage={onRegisterPage} />
-            <Buttons onRegisterPage={onRegisterPage} />
-            <Prompt
-              onRegisterPage={onRegisterPage}
-              onClick={() => {
-                setModalState(
-                  onRegisterPage ? ModalState.LOGIN : ModalState.REGISTER,
-                );
-              }}
-            />
-            <Footer onRegisterPage={onRegisterPage} />
+      <div className="fixed inset-0 z-40 bg-[#ffffff] bg-opacity-50 backdrop-blur-sm" />
+      <Dialog defaultOpen={true}>
+        <DialogContent>
+          <div className="fixed inset-0 z-50 flex items-center justify-center">
+            <div className="relative flex h-full w-full flex-col justify-center gap-10 bg-white md:h-[42rem] md:w-[38rem] md:rounded-lg md:px-14 md:py-16 md:shadow-lg">
+              {/* Move the close icon to the top-left */}
+              <div className="absolute top-4 right-4">
+                <DialogClose>
+                  <CloseIcon />
+                </DialogClose>
+              </div>
+              <Header onRegisterPage={onRegisterPage} />
+              <Buttons onRegisterPage={onRegisterPage} />
+              <Prompt
+                onRegisterPage={onRegisterPage}
+                onClick={() => {
+                  setModalState(
+                    onRegisterPage ? ModalState.LOGIN : ModalState.REGISTER,
+                  );
+                }}
+              />
+              <Footer onRegisterPage={onRegisterPage} />
+            </div>
           </div>
-        </div>
+        </DialogContent>
       </Dialog>
     </>
   );
