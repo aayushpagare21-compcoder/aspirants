76c952cc9d29efe56c57d29c21dd309222c7020d
Aayush Pagare <aayushpagare21@gmail.com>
2024-11-03 21:15:11 +0530

    fix:authentication

--- src/auth.ts
+++ src/auth.ts
@@ -9,7 +9,12 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
   },
   adapter: PrismaAdapter(prisma),
   secret: process.env.NEXTAUTH_SECRET,
-  providers: [Google],
+  providers: [
+    Google({
+      clientId: process.env.AUTH_GOOGLE_ID! as string,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET! as string,
+    }),
+  ],
   pages: {
     signIn: "/login",
   },
