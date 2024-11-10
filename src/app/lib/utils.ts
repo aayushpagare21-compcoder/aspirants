08431962eea7dfbe52d20cb329ce66de77f53cf4
Aayush Pagare <aayushpagare21@gmail.com>
2024-11-10 15:21:28 +0530

    chore:remove-unused-code

--- src/app/lib/utils.ts
+++ src/app/lib/utils.ts
@@ -5,14 +5,4 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-export async function getAPI<T>(path: string): Promise<T> {
-  const resp = await fetch(`${process.env.NEXT_BASE_URL}/api/${path}`, {
-    ...(process.env.NODE_ENV === "development" && {
-      cache: "no-store",
-    }),
-  });
 
-  const data = await resp.json();
-
-  return data as T;
-}
