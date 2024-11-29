fe86a9c7d3bafd81539ccf476fbd55f4a8df3eff
Aayush Pagare <aayushpagare21@gmail.com>
2024-11-30 00:07:08 +0530

    feat:add-rate-limting

--- src/app/components/shared/Logo/AspirantsLogo.tsx
+++ src/app/components/shared/Logo/AspirantsLogo.tsx
@@ -11,7 +11,7 @@ export const AspirantsLogo = ({
     "font-[family-name:var(--font-gtsuper-medium)] text-[1.6rem] md:text-[1.8rem] font-bold";
   return (
     <Link href="/">
-      <div className={cn(defaultClasses, overrideClasses)}>AspirantAI</div>;
+      <div className={cn(defaultClasses, overrideClasses)}>AspirantAI</div>
     </Link>
   );
 };
