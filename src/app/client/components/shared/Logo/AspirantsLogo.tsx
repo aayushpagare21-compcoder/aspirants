32d8e67da9020f6b5e6b2f2b259243a2703f98f8
Aayush Pagare <aayushpagare21@gmail.com>
2024-12-11 15:09:21 +0530

    add the logo

--- src/app/client/components/shared/Logo/AspirantsLogo.tsx
+++ src/app/client/components/shared/Logo/AspirantsLogo.tsx
@@ -1,15 +1,25 @@
 import React from "react";
 import { cn } from "@/app/lib/utils";
+import Image from "next/image";
 export const AspirantsLogo = ({
   overrideClasses,
 }: {
   overrideClasses?: string;
 }) => {
   const defaultClasses =
-    "font-[family-name:var(--font-gtsuper-medium)] text-[1.6rem] md:text-[1.8rem] font-bold";
+    "flex font-[family-name:var(--font-gtsuper-medium)] md:text-[1.4rem] font-bold gap-1 items-center";
   return (
     <div className={cn(defaultClasses, overrideClasses)}>
-      <span className="text-tertiary">Aspirant</span>AI
+      <Image
+        className="w-12 h-12"
+        src="/android-chrome-512x512.png"
+        width={60}
+        height={40}
+        alt="aspirantsai logo"
+      />
+      <div className="hidden md:flex mt-1">
+        <span className="text-tertiary">Aspirants</span>AI
+      </div>
     </div>
   );
 };
